de1808c2e7261658a47e38f54819f875c341fa49
Artem Chychkan <a.i.chychkan@gmail.com>
2022-02-18 20:44:06 +0100

    Add JWTTokenProvider.

--- src/main/java/com/example/hospital/constants/SecurityConstants.java
+++ src/main/java/com/example/hospital/constants/SecurityConstants.java
@@ -5,7 +5,7 @@ public class SecurityConstants {
     public final static String TOKEN_PREFIX = "Bearer "; // a person who has this token-prefix has the rights to be authenticated without real token;
     public final static String JWT_TOKEN_HEADER = "JWT-Token"; // custom header to which real token will be attached
     public final static String TOKEN_HEADER_CANNOT_BE_VERIFIED = "Token cannot be verified";
-    public final static String GET_ARRAYS_APPLE = "Token issued by Apple"; // issuer of the token
+    public final static String GET_ARRAYS_COMPANY = "Token issued by Apple"; // issuer of the token
     public final static String GET_ARRAYS_ADMINISTRATION = "Token issued to Administration"; // issued to the specific audience e.g. admins
     public static final String AUTHORITIES = "authorities"; // user authorities
     public static final String FORBIDDEN_MESSAGE = "Forbidden to access the page.Please, log in"; // a person got a token but is forbidden to access the resource
